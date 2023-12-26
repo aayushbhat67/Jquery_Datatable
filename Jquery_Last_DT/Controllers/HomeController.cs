a16cc3215e5810c25e1cd58f1db7f1cdfbfba2af
aayushbhat67 <aayushbhat67@gmail.com>
2023-12-26 16:29:16 +0530

    Initial commit

---
+++ Jquery_Last_DT/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jquery_Last_DT.Models;
+
+
+namespace Jquery_Last_DT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult ClientDetail()
+        {
+            JQLastEntities1 db = new JQLastEntities1();
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Customer> Customer = db.Customers.ToList();
+            return Json(new { data = Customer }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Jquery_Last_DT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jquery_Last_DT.MvcApplication" Language="C#" %>
